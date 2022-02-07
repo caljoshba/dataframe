53e19072e3382b6bccc167126caabc8c93414c41
Callum Barry <callum.barry@cirium.com>
2022-02-07 16:27:21 +0000

    adding unit tests for row

--- src/row/mod.rs
+++ src/row/mod.rs
@@ -48,3 +48,60 @@ impl PartialEq for Row {
 impl Eq for Row {}
 
 pub type RcRow = Rc<RefCell<Row>>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::cell::{
+        types::datatypes::AnyType,
+        Cell,
+        RcCell,
+    };
+    #[test]
+    fn add_cell() {
+        let value: AnyType = 67u16.into();
+        let row: RcRow = Row::new(3);
+        let cell: RcCell = Cell::new(value, &row, "timmeh");
+
+        row.borrow_mut().add_cell(&cell);
+
+        assert!(cell == row.borrow().get_cells()[0].upgrade().unwrap());
+    }
+
+    #[test]
+    fn add_cell_ref_count() {
+        let value: AnyType = 67u16.into();
+        let row: RcRow = Row::new(3);
+        let cell: RcCell = Cell::new(value, &row, "timmeh");
+
+        row.borrow_mut().add_cell(&cell);
+
+        assert!(1 == row.borrow().get_cells()[0].strong_count());
+        assert!(1 == row.borrow().get_cells()[0].weak_count());
+    }
+
+    #[test]
+    fn drop_cell() {
+        let value: AnyType = 67u16.into();
+        let row: RcRow = Row::new(3);
+        let cell: RcCell = Cell::new(value, &row, "timmeh");
+
+        row.borrow_mut().add_cell(&cell);
+        assert!(row.borrow().get_cells().len() == 1);
+
+        row.borrow_mut().drop_cell(&cell);
+        assert!(row.borrow().get_cells().len() == 0);
+    }
+
+    #[test]
+    fn update_index() {
+        let value: AnyType = 67u16.into();
+        let row: RcRow = Row::new(3);
+        let cell: RcCell = Cell::new(value, &row, "timmeh");
+
+        row.borrow_mut().add_cell(&cell);
+        row.borrow_mut().update_index(7);
+        assert!(row.borrow().index == 7);
+    }
+
+}
